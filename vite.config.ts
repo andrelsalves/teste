298a6433532eb0b5b87d808b8c2ee22c74b2708b
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-02 19:09:15 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }: ConfigEnv) => {
   // Carrega as variáveis de ambiente (.env) baseado no modo (dev/prod)
