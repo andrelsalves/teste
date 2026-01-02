32010f665688a3dd7554527db2dfa5198487bc7a
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-02 19:19:29 -0300

    Update TechDashboard.tsx

--- views/TechDashboard.tsx
+++ views/TechDashboard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Icons } from "@/components/constants/icons";
+import { Icons } from "@/components/constants/icons.tsx";
 import { AppointmentStatus } from '../types/types';
 import SignatureCanvas from 'react-signature-canvas';
 import NewAppointmentModal from '../components/modal/NewAppointmentModal';
