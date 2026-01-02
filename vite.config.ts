8c0f3bd5e94960c195e9248d0cf4f601adc4d520
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-02 19:34:34 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }: ConfigEnv) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, 'src'),
+      
       },
       dedupe: ['react', 'react-dom']
     },
