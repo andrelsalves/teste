4f101cc6385bdaba067c8ce484f29e39a2a97ebf
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-02 19:47:13 -0300

    Delete types directory

--- types/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly API_KEY: string;
-    readonly VITE_SUPABASE_URL: string;
-    readonly VITE_SUPABASE_ANON_KEY: string;
-  }
-}
