26e923aed6a20fa9227ed37e330bdf5e6343a391
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-02 18:58:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig, loadEnv, ConfigEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // <--- IMPORTANTE: Adicione esta linha
-import { fileURLToPath } from 'url'; // <--- Necessário para o __dirname em ESM
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Recriando o __dirname para que funcione em qualquer ambiente (Local e Render)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }: ConfigEnv) => {
-  // Carrega as variáveis de ambiente (.env) baseado no modo (dev/prod)
+
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
@@ -23,7 +22,6 @@ export default defineConfig(({ mode }: ConfigEnv) => {
     },
     resolve: {
       alias: {
-        // Agora o 'path' e '__dirname' estão definidos!
         '@': path.resolve(__dirname, './src'),
       },
       dedupe: ['react', 'react-dom']
