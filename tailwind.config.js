541c4ab6f4e72b15a4d3075d8d76685748b22526
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-03 20:16:14 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
-export default {
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Garanta que esta linha existe
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
