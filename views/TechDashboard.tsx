0c71aa580b2464fda733624dea485794724827c9
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-02 23:40:02 -0300

    Update TechDashboard.tsx

--- views/TechDashboard.tsx
+++ views/TechDashboard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Icons } from "../components/constants/icons";
 import SignatureCanvas from 'react-signature-canvas';
 import NewAppointmentModal from '../components/modal/NewAppointmentModal';
+import { reportService } from '../services/reportService';
 
 interface TechDashboardProps {
     user: { id: string; name: string; email?: string; };
