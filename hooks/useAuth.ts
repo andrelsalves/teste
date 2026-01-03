38dd7108aa59fb189975d4a58d86b43bd0a47dd5
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-03 19:15:48 -0300

    Update useAuth.ts

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,14 +1,10 @@
-// src/hooks/useAuth.ts
 import { useContext } from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-
+import { AuthContext } from '../providers/AuthProvider';
 
 export function useAuth() {
   const context = useContext(AuthContext);
-
   if (!context) {
     throw new Error('useAuth deve ser usado dentro de AuthProvider');
   }
-
   return context;
 }
