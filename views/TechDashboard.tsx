1706530e586c0b14f6686d2b52fdb833b8fe05b7
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-02 22:43:47 -0300

    Update TechDashboard.tsx

--- views/TechDashboard.tsx
+++ views/TechDashboard.tsx
@@ -86,7 +86,7 @@ const TechDashboard: React.FC<TechDashboardProps> = ({
                                 {app.company_name}
                             </h3>
                              {/* Texto motivos de visita */}
-                            <p className="text-slate-200 text-[150px] fonte-white mt-50 italic line-clamp-1">
+                            <p className="text-slate-200 text-[15px] fonte-white mt-50 italic line-clamp-1">
                                 "{app.reason}"
                             </p>
 
