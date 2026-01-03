f04ad4329a0525745734efd9f82a1b8278ebdb0e
andrelsalves <132823492+andrelsalves@users.noreply.github.com>
2026-01-03 19:06:20 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from './providers/AuthProvider';
 import { UIDensityProvider } from './contexts/UIDensityContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
